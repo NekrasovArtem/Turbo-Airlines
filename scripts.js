b20bdccdb8ed8bfab45a6d6a574cf1618c021bf6
NekrasovArtem <113281369+NekrasovArtem@users.noreply.github.com>
2023-02-08 23:35:12 +0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,40 +1,37 @@
 // Функция скрытия навигационного меню в шапке
 function functionMenu() {
-    var x = document.getElementById("myNavigation");
-    if (x.className === "navigation") {
-        x.className += " responsive";
-    } else {
-        x.className = "navigation";
-    }
+  var x = document.getElementById("myNavigation");
+  if (x.className === "navigation") {
+    x.className += " responsive";
+  } else {
+    x.className = "navigation";
+  }
 }
 
 // Функция принципа накопления бронирований
-function principles(){
+function principles() {
+  var priceblock = document.getElementById("booking__form-price");
+  var probblock = document.getElementById("booking__form-prob");
+  var priceinput = document.getElementById("priceinput").value;
+  var probinput = document.getElementById("probinput").value;
 
-	var price = document.getElementById('booking__form-price');
-	var prob = document.getElementById('booking__form-prob');
-
-	var a = document.getElementById('priceinput').value;
-	var b = document.getElementById('probinput').value;
-
-	if(a > 0 && a <= 10){
-		price.style.width = "50%";
-	}
-	else if(a >= 10 && a <= 20){
-		price.style.width = "30%";
-	}
-	else{
-		price.style.width = "100%";
-	}
-
-	if(b > 0 && b <= 5){
-		prob.style.width = "30%";
-	}
-	if(b >= 5 && b <= 10){
-			prob.style.width = "50%";
-		}
-	if(b >= 10 && b <= 15){
-			prob.style.width = "100%";
-		}
-
+  if (priceinput > 0 && priceinput <= 10) {
+    priceblock.style.width = "50%";
+  } else if (priceinput >= 10 && priceinput <= 20) {
+    priceblock.style.width = "30%";
+  } else {
+    priceblock.style.width = "100%";
+  }
+  if (probinput > 0 && probinput <= 11) {
+    probblock.style.width = "25%";
+  }
+  if (probinput >= 12 && probinput <= 23) {
+    probblock.style.width = "50%";
+  }
+  if (probinput >= 24 && probinput <= 35) {
+	probblock.style.width = "75%"
+  }
+  if (probinput >= 36 && probinput <= 48) {
+    probblock.style.width = "100%";
+  }
 }
